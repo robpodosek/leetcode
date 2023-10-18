0cfbb2fcfce8a413839ba14a30374e8e97306c16
robpodosek <rpodosek@gmail.com>
2023-10-18 11:12:35 -0700

    Valid Anagram

---
+++ 242.valid-anagram.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=242 lang=python3
+#
+# [242] Valid Anagram
+#
+
+# @lc code=start
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        sMap, tMap = {}, {}
+
+        for i in range(len(s)):
+            print(s[i], t[i])
+            sMap[s[i]] = sMap.get(s[i], 0) + 1
+            tMap[t[i]] = tMap.get(t[i], 0) + 1
+        return sMap == tMap
+
+
+# @lc code=end
