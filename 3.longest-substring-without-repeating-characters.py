23809fa7b4e751843755280c7a7c19b4223db593
robpodosek <rpodosek@gmail.com>
2024-03-07 10:27:49 -0800

    Rewrite leetcode 3

--- 3.longest-substring-without-repeating-characters.py
+++ 3.longest-substring-without-repeating-characters.py
@@ -7,21 +7,19 @@
 # @lc code=start
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        charSet = set()
-        left = 0
-        maxLength = 0
+        l, longest = 0, 0
+        letters = set()
 
-        for right in range(len(s)):
-            if s[right] not in charSet:
-                charSet.add(s[right])
-                maxLength = max(maxLength, right - left + 1)
-            else:
-                # move the left pointer forward, removing characters from the set until the repeating character is no longer present
-                while s[right] in charSet:
-                    charSet.remove(s[left])
-                    left += 1
-                charSet.add(s[right])
+        for r in range(len(s)):
+            # if it's in the letters then this substring is invalid
+            while s[r] in letters:
+                letters.remove(s[l])
+                l += 1
 
-        return maxLength
+            letters.add(s[r])
+            w = r - l + 1
+            longest = max(longest, w)
+
+        return longest
 
 # @lc code=end
