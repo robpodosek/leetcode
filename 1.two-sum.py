bdc234422210fcc2781417a3fc0991e758e29527
robpodosek <rpodosek@gmail.com>
2024-03-13 10:57:07 -0700

    Rework two sum

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -7,11 +7,18 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # create a hashmap where
+        # key: number value in array
+        # value: index position in array
         found = {}
-        for i, n in enumerate(nums):
-            diff = target - n
-            if diff in found:
-                return [found[diff], i]
-            found[n] = i
+        for idx, num in enumerate(nums):
+            print(idx, num)
+            complement = target - num
+            if complement in found:
+                # if the difference between target and current number is in the map, then we've found a match and can return both indices
+                return [found[complement], idx]
+
+            # Log the number and its index
+            found[num] = idx
 
 # @lc code=end
