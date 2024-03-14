2c588b84077ed55e656ff44cc1e07b516b21071b
robpodosek <rpodosek@gmail.com>
2024-03-14 09:33:33 -0700

    Rework [21] Merge Two Sorted Lists

--- 21.merge-two-sorted-lists.py
+++ 21.merge-two-sorted-lists.py
@@ -6,28 +6,27 @@
 
 # @lc code=start
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        tail = dummy
+        # dummy is a placeholder where dummy.next always stays pointed to the head of the list while curr advances
+        dummy = curr = ListNode()
 
         while list1 and list2:
             if list1.val < list2.val:
-                tail.next = list1
-                list1 = list1.next
+                curr.next = list1
+                list1 = list1.next  # we used up the list1 node so advance it
             else:
-                tail.next = list2
-                list2 = list2.next
-            tail = tail.next
+                curr.next = list2
+                list2 = list2.next  # we used up the list2 node so advance it
+            curr = curr.next  # always advance the current node pointer
 
-        if list1:
-            tail.next = list1
-        elif list2:
-            tail.next = list2
+        curr.next = list1 if list1 else list2
 
         return dummy.next
 
